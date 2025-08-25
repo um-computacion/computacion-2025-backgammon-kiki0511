62a95b96bc466abcd2c2ba51bff6fbb949aa85d9
Joaquín <kiki0511@example.com>
2025-08-25 10:50:23 -0300

    borre contenido de clase checker, dice, player para organizar las ramas de mejor manera

--- core/checker.py
+++ core/checker.py
@@ -1,14 +1,1 @@
-class Checker:
-    """
-    Una ficha del juego de Backgammon.
-    Cada ficha pertenece a un jugador.
-    """
-    
-    def __init__(self, owner):
-        self.owner = owner
-    
-    def get_owner(self):
-        return self.owner
-    
-    def __str__(self):
-        return f"Ficha del jugador {self.owner}"
+## borrado para organizar ramas 
